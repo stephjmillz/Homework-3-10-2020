e547bf00a276e6fbf95d77790c54201c4952673d
Steph J Millz <stephjmillz@outlook.com>
2020-03-11 21:11:59 -0700

    files to commit

---
+++ src/main/java/firstpackage/Main.java
@@ -0,0 +1,93 @@
+package firstpackage;
+
+public class Main {
+/*3 ints. 2 subtract & then divide
+   // public static void printNumber(int number, int number6, int number7) {
+    // System.out.println((number - number6) / number7 );
+
+   // }
+
+    // public static void main(String[] args) {
+      //  printNumber(25, 5, 4);
+
+
+//Price $200 - 30%
+        //30% of $200 = $60
+
+    //$200 - $60 = $140
+
+        public static void priceNumber(int number, int number2) {
+            System.out.println((number * number2) / 100);
+
+        }
+
+            public static void main(String[] args) {
+                priceNumber(30, 200);
+
+
+        }
+
+
+
+        }
+*/
+     /*   public static void main(String[] args) {
+        int x = 20;
+        int y = 35;
+        System.out.println(x);
+        System.out.println(y);
+
+        }
+}
+
+/*PRimitive Data Types
+boolean - false (true or false)
+byte - '\u0000' 0 to 65535
+short 0 -32768 to 32767
+char
+int
+long
+float
+double
+
+
+
+     public static void main(String[] args) {
+
+     System.out.println(Integer.MIN_VALUE);
+             System.out.println(Integer.MAX_VALUE);
+     }
+}
+
+*/
+     public static void main(String[] args) {
+
+         byte b =100;
+         short s =123;
+         int v = 123543;
+         int calc = -9876345;
+         long amountVal = 1234567891;
+         float intrestRate = 12.25f;
+         double sineVal = 12345.234d;
+         boolean flag = true;
+         boolean val = false;
+         char ch1 = 88; // code for X
+         char ch2 = 'Y';
+         System.out.println("byte Value = "+ b);
+         System.out.println("short Value = "+ s);
+         System.out.println("int Value = "+ v);
+         System.out.println("int second Value = "+ calc);
+         System.out.println("long Value = "+ amountVal);
+         System.out.println("float Value = "+ intrestRate);
+         System.out.println("double Value = "+ sineVal);
+         System.out.println("boolean Value = "+ flag);
+         System.out.println("boolean Value = "+ val);
+         System.out.println("char Value = "+ ch1);
+         System.out.println("char Value = "+ ch2);
+
+int intNumber = 45;
+        long longNumber = intNumber;
+        System.out.println(longNumber);
+
+     }
+}
